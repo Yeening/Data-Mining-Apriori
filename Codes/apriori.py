f5f5a0d2b768993d8f591a6c43a44f3c1b8c4327
Yeening <yl7sr@virginia.edu>
2019-09-08 22:45:59 -0400

    finished get frequent itemsets

--- Codes/apriori.py
+++ Codes/apriori.py
@@ -1,7 +1,14 @@
-from main import *
+# from main import *
+
+
+count = 0
+frequent_sets = []
 
 
 def generate_item_set(transactions, support):
+    global count, frequent_sets
+    count = 0
+    frequent_sets = []
     support_count = support * len(transactions)
     item_dic = {}
 
@@ -13,36 +20,46 @@ def generate_item_set(transactions, support):
             else:
                 item_dic[item] += 1
 
+    # filter the item set for the first time
+    # get the frequent item set with length 1
     fre_item_1 = {}
-    for item in item_dic.keys():
+    for item in item_dic:
         if item_dic[item] >= support_count:
             fre_item_1[item] = item_dic[item]
 
-    print(len(fre_item_1),fre_item_1,'\n')
-    generate(fre_item_1, support, 0, transactions)
+    # print(len(fre_item_1),fre_item_1,'\n')
+    generate(fre_item_1, support_count, 0, transactions, 1)
+    return frequent_sets, count
 
 
-def generate(item_dic, support, count, transactions):
+def generate(item_dic, support_count, _count, transactions, length):
+    # add the count of current frequent item set
+    global count,frequent_sets
     count += len(item_dic)
-    support_count = support * len(transactions)
+    frequent_sets.append(item_dic)
+    # print("number of length-" + str(length) + " frequent itemsets: ")
+    # frequent item set of next level
     next_dic = {}
-    for item1 in item_dic.keys():
-        for item2 in item_dic.keys():
-            if item1 == item2:
+    for item1 in item_dic:
+        for item2 in item_dic:
+            if item1 >= item2:  # skip equal
                 continue
+            # get list from string
             item_set1 = item1.split(',')
             item_set2 = item2.split(',')
+            # only combine when set1 and set2 have same items except the last one
             if item_set1[:-1] == item_set2[:-1]:
                 temp_count = 0
                 for transaction in transactions:
-                    if contain(transaction,item_set1) and contain(transaction, item_set2):
+                    # transaction contains both set1 and set2
+                    if contain(transaction, item_set1) and contain(transaction, item_set2):
                         temp_count += 1
                 if temp_count >= support_count:
-                    next_dic[item1+','+item2] = temp_count
-    print(len(next_dic.keys()), next_dic)
+                    next_dic[item1+','+str(item_set2[-1])] = temp_count
+    # print(len(next_dic.keys()), next_dic)
     if len(next_dic) > 0:
-        generate(next_dic, support, count, transactions)
-    return count
+        generate(next_dic, support_count, count, transactions, length+1)
+    # return count
 
 
 def contain(transaction, item_set):
@@ -51,4 +68,6 @@ def contain(transaction, item_set):
             return False
     return True
 
-generate_item_set(data_to_array(), 0.5)
+
+# print(generate_item_set(data_to_array(), 0.5))
+
