031af67ed1c0ec239d02f14b67b671238cf007be
Leizhen Shi <ls4056@nyu.edu>
2019-09-11 20:49:41 -0400

    Adding Templates

--- Codes/apriori.py
+++ Codes/apriori.py
@@ -1,8 +1,10 @@
-# from main import *
+from main import data_to_array
 
 
 count = 0
 frequent_sets = []
+heads = []
+bodies = []
 
 
 def generate_item_set(transactions, support):
@@ -69,5 +71,200 @@ def contain(transaction, item_set):
     return True
 
 
-# print(generate_item_set(data_to_array(), 0.5))
+def confidence(rule_set, head_set, frequent_sets):
+    if len(head_set) == 0 or len(rule_set) == len(head_set):
+        return 0
+    # print(frequent_sets[len(lst1)-1])
+    count_lst1 = float(frequent_sets[len(rule_set)-1].get(','.join(rule_set)))
+    # print(frequent_sets[len(lst2) - 1])
+    count_lst2 = float(frequent_sets[len(head_set)-1].get(','.join(head_set)))
+    return count_lst1 / count_lst2
 
+
+# # apriori algorithm for association rule
+# def get_deeper(lhs, rhs, res, confidence_threshold, frequent_sets):
+#     parent_list = lhs
+#     for i in lhs:
+#         rhs.add(i)
+#         temp_lhs = intersection(lhs, rhs)
+#         if (confidence(parent_list, temp_lhs, frequent_sets) > confidence_threshold):
+#             res[','.join(lhs) + "->" + ','.join(rhs)] = 1  # adding the rules to the stored value
+#             get_deeper(lhs, rhs, res, confidence_threshold, frequent_sets)
+
+
+def get_deeper(left, right, _confidence, associations, frequent_sets):
+    if len(left) == 0:
+        return
+    rule = left + right
+    rule.sort()
+    left.sort()
+    if confidence(rule, left, frequent_sets) > _confidence:
+        associations.add(str(left+['-']+right))
+    for item in left:
+        left.remove(item)
+        right.append(item)
+        get_deeper(left,right,_confidence, associations, frequent_sets)
+        left.append(item)
+        right.remove(item)
+
+
+def generate_associations(_confidence, support, transactions):
+    frequent_sets, _count = generate_item_set(data_to_array(), support)
+    associations = set({})
+    for item_sets in frequent_sets[1:]:
+        for i in item_sets:
+            item_set = i.split(',')
+            left = item_set
+            right = []
+            get_deeper(left, right, _confidence, associations, frequent_sets)
+    # print(associations)
+    return associations
+
+def ass_str_to_list(s):
+    s1 = s.strip('[').strip(']')
+    l = s1.split(',')
+    left = []
+    right = []
+    block = 0
+    for i in range(len(l)):
+        if '-' in l[i]:
+            block = i
+    for i in range(len(l)):
+        if i < block:
+            left.append(l[i].lstrip()[1:-1])
+        if i > block:
+            right.append(l[i].lstrip()[1:-1])
+    return left, right
+
+#Part 2 template 1
+def template1(position, number, combination): #combination should be a set
+    position = position.upper()  # avoid any lower case input
+    if (type(number) == str): #change number to all upper case letters
+        number.upper()
+
+    if number == "NONE":
+        if position == "RULE":
+            counter = 0
+            for i in range(len(heads)):
+                head_line = [item for item in heads[i] if item in combination]
+                body_line = [item for item in bodies[i] if item in combination]
+                if (len(head_line) > 0 or len(body_line) > 0):
+                    counter += 1
+            print(len(heads) - counter)
+
+        elif position == "HEAD":
+            counter = 0
+            for i in range(len(heads)):
+                head_line = [item for item in heads[i] if item in combination]
+                if (len(head_line) > 0):
+                    counter += 1
+            print(len(heads) - counter)
+
+        elif position == "BODY":
+            counter = 0
+            for i in range(len(bodies)):
+                body_line = [item for item in heads[i] if item in combination]
+                if (len(body_line) > 0):
+                    counter += 1
+            print(len(bodies) - counter)
+
+    elif number == "ANY":
+        if position == "RULE":
+            counter = 0
+            for i in range(len(heads)):
+                head_line = [item for item in heads[i] if item in combination]
+                body_line = [item for item in bodies[i] if item in combination]
+                if (len(head_line) > 0 or len(body_line) > 0):
+                    counter += 1
+            print(counter)
+
+        elif position == "HEAD":
+            counter = 0
+            for i in range(len(heads)):
+                head_line = [item for item in heads[i] if item in combination]
+                if (len(head_line) > 0):
+                    counter += 1
+            print(counter)
+
+        elif position == "BODY":
+            counter = 0
+            for i in range(len(bodies)):
+                body_line = [item for item in heads[i] if item in combination]
+                if (len(body_line) > 0):
+                    counter += 1
+            print(counter)
+    else:
+        if position == "RULE":
+            counter = 0
+            for i in range(len(heads)):
+                head_line = [item for item in heads[i] if item in combination]
+                body_line = [item for item in bodies[i] if item in combination]
+                if (len(head_line) + len(body_line) == number):
+                    counter += 1
+            print(counter)
+
+        elif position == "HEAD":
+            counter = 0
+            for i in range(len(heads)):
+                head_line = [item for item in heads[i] if item in combination]
+                if (len(head_line) == number):
+                    counter += 1
+            print(counter)
+
+        elif position == "BODY":
+            counter = 0
+            for i in range(len(bodies)):
+                body_line = [item for item in heads[i] if item in combination]
+                if (len(body_line) == number):
+                    counter += 1
+            print(counter)
+
+#Part 2 Template 2
+def template2(position, number):
+    position = position.upper()
+
+    if position == "RULE":
+        counter = 0
+        for i in range (len(heads)):
+            if (len(heads[i]) + len(bodies[i]) >= number):
+                counter += 1
+        print(counter)
+
+    elif position == "HEAD":
+        counter = 0
+        for i in range(len(heads)):
+            if (len(heads[i]) >= number):
+                counter += 1
+        print(counter)
+
+    else:
+        counter = 0
+        for i in range(len(bodies)):
+            if (len(bodies[i]) >= number):
+                counter += 1
+        print(counter)
+
+#Part 2 template 3
+
+
+
+# s = "['G82_DOWN', '-', 'G72_UP', 'G59_UP']"
+#
+# left,right = ass_str_to_list(s)
+# print(left,right)
+associations = generate_associations(0.7, 0.5, generate_item_set(data_to_array(), 0.5))
+# associations= generate_associations(0.7, 0.5, generate_item_set())
+for association in associations:
+    left, right = ass_str_to_list(association)
+    heads.append(left)
+    bodies.append(right)
+
+counter = 0
+for i in range(len(heads)):
+    counter += 1
+    print(str(heads[i]) + "->" + str(bodies[i]))
+print(counter)
+
+template1("RULE", "NONE", {"G96_DOWN", "G59_UP"})
+
+template2("rule", 3)
